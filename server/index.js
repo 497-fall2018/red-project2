c29300c6a16d3737401043355df0ae0e215e7b11
abizzaar <richmond.alayna@gmail.com>
2018-11-02 10:36:09 -0500

    graphQL schema defined: Dining, Menu, Food, and User types; queries and mutations

--- server/index.js
+++ server/index.js
@@ -11,26 +11,76 @@ const User = require('./models/user')
 // typeDefs is the schema (what requests you can make to the server)
 // resolvers implement how our requests will be handled
 typeDefs = `
-    type Query {
-        diningHalls: [DiningHall]
-    }
-    type DiningHall {
+    type Dining {
         id: ID!
         name: String!
         rating: Int!
         hours: String!
+        isHall: Boolean!
+        menus: [Menu!]!
+        foods: [Food!]!
+    }
+    type Menu {
+        id: ID!
+        date: String!
+        timeOfDay: String!
+        dining: Dining!
+        foods: [Food!]!
+    }
+    type Food {
+        id: ID!
+        name: String!
+        thumbsUp: Int!
+        thumbsDown: Int!
+        diet: String
+        preference: [String!]!
+        dining: Dining!
+    }
+    type User {
+        id: ID!
+        name: String!
+        diet: String
+        preferences: [String!]!
+        faveFoods: [Food!]!
 
     }
+    type Query {
+        dinings: [DiningHall!]!
+        diningHall:
+    }
     type Mutation {
-        createDiningHall(name: String!, hours: String!): DiningHall
-        updateDiningHall(id: ID!, rating: Int!): Boolean
-        removeDiningHall(id: ID!): Boolean
+        createDining(name: String!, hours: String, isHall: Boolean!): Dining!
+        updateDining(id: ID!, rating: Int, hours: Int): Boolean!
+        removeDining(id: ID!): Boolean!
+
+        createMenu(dining: Dining!, timeOfDay: String, date: String, foods: [Food!]): Menu!
+        updateMenu(id: ID!, foods: [Food!], date: String): Boolean!
+        removeMenu(id: ID!): Boolean!
+
+        createFood(name: String!, diet: String, dining: Dining!): Food!
+        updateFood(id: ID!, thumbsUp: Int, thumpsDown: Int): Boolean!
+        removeFood(id: ID!): Boolean!
+
+        createUser(name: String!, diet: String, preferences: [String!]): User!
+        updateUser(id: ID!, name: String, diet: String, preferences: [String!]): Boolean!
+        updateFaveFoods(id: ID!, faveFoods: [Foods!]!): Boolean!
+        removeUser(id: ID!): Boolean!
     }
 `
 // mongo generates an id by default, ID is a special type in graphql
 // passing the id in an update function is how graphQL knows which item to update
 
 resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+
+    }
+    
+    
+    /*
     Query: {
         diningHalls: () => DiningHall.find()
     },
@@ -49,6 +99,7 @@ resolvers = {
             return True;
         }
     }
+    */
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers });
