9c32b449971158eafa1f0bd79d98041ada72148a
abizzaar <richmond.alayna@gmail.com>
2018-11-15 13:22:17 -0600

    displayed correct diningHallTopFoods with Nirmal

--- client/src/components/diningMenuPage.js
+++ client/src/components/diningMenuPage.js
@@ -14,23 +14,31 @@ export class DiningMenuPage extends Component {
     }
   }
   render() {
-    console.log('xxxxxxxx')
-    console.log( this.props.diningHallTopFoods[0].topFoods)
-    return (
-      <div>
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-          <div style={{ margin: "auto", width: 900 }}>
-            <Header as='h1'>{this.props.diningHall}</Header>
-            <Header as='h1'>Top 5</Header>
-              <Top5Table
-                foodItems= { this.props.diningHallTopFoods[0].topFoods }
-                handleThumbsUp= {this.props.handleThumbsUp}
-                handleThumbsDown= {this.props.handleThumbsDown}
-            />
+    console.log('aaaa')
+    console.log(this.props.diningHall)
+    if (this.props.diningHallTopFoods.length != 0) {
+      return (
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+            <div style={{ margin: "auto", width: 900 }}>
+              <Header as='h1'>{this.props.diningHall}</Header>
+              <Header as='h1'>Top 5</Header>
+                <Top5Table
+                  foodItems= { this.props.diningHallTopFoods.filter(
+                    (menu) => menu.dining.name == this.props.diningHall).map(
+                      (menu) => menu.topFoods)[0]
+                  }
+                  handleThumbsUp= {this.props.handleThumbsUp}
+                  handleThumbsDown= {this.props.handleThumbsDown}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return null
+    }
   }
 }
 
--- client/src/components/top5Table.js
+++ client/src/components/top5Table.js
@@ -5,22 +5,25 @@ import FoodRow from './foodRow';
 export class Top5Table extends Component {
 
     render() {
-        console.log('yyyy')
-        console.log(this.props.foodItems)
-        return (
-            <Card.Group> {
-                this.props.foodItems
-                .map(f => {
-                    return <FoodRow
-                        key={f.id}
-                        food={f}
-                        rank = {"some rank"} //rank = { position in array }
-                        handleThumbsUp={this.props.handleThumbsUp}
-                        handleThumbsDown={this.props.handleThumbsDown}
-                        />
-                })
-            } </Card.Group>
-        )
+        if (this.props.foodItems != null) {
+            return (
+                <Card.Group> {
+                    this.props.foodItems
+                    .map(f => {
+                        return <FoodRow
+                            key={f.id}
+                            food={f}
+                            rank = {"some rank"} //rank = { position in array }
+                            handleThumbsUp={this.props.handleThumbsUp}
+                            handleThumbsDown={this.props.handleThumbsDown}
+                            />
+                    })
+                } </Card.Group>
+            )
+        }
+        else {
+            return null
+        }
     }
 }
 
